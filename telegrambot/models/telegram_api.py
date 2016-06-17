8944815136f366ff3dbb6e9de9281d04f1a7d226
Oleksandr Shcherbakov <admin@sp06n.org.ua>
2016-06-17 18:29:41 +0200

    Added models for different Tg types

--- telegrambot/models/telegram_api.py
+++ telegrambot/models/telegram_api.py
@@ -47,6 +47,129 @@ class Chat(models.Model):
         return hasattr(self, 'auth_token') and not self.auth_token.expired()
 
 @python_2_unicode_compatible
+class Audio(models.Model):
+    file_id = models.CharField(max_length=255, primary_key=True, verbose_name=_("file_id"))
+    duration = models.IntegerField(default=0, verbose_name=_("Duration"))
+    # Optionals
+    performer = models.CharField(max_length=255, null=True, blank=True, verbose_name=_("Performer"))
+    title = models.CharField(max_length=255, null=True, blank=True, verbose_name=_("Title"))
+    mime_type = models.CharField(max_length=255, null=True, blank=True, verbose_name=_("MIME type"))
+    file_size = models.IntegerField(default=0, null=True, blank=True, verbose_name=_("File size"))
+    class Meta:
+        verbose_name = 'Audio'
+        verbose_name_plural = 'Audios'
+    def __str__(self):
+        return "%s - %s (%d %s)" % (self.performer or 'Unknown artist', self.title or 'Unknown title', self.duration, _('sec.'))
+
+@python_2_unicode_compatible
+class PhotoSize(models.Model):
+    file_id = models.CharField(max_length=255, primary_key=True, verbose_name=_("file_id"))
+    width = models.IntegerField(default=0, verbose_name=_("Width"))
+    height = models.IntegerField(default=0, verbose_name=_("Height"))
+    # Optionals
+    file_size = models.IntegerField(default=0, null=True, blank=True, verbose_name=_("File size"))
+    class Meta:
+        verbose_name = 'PhotoSize object'
+        verbose_name_plural = 'PhotoSize objects'
+    def __str__(self):
+        return "%s (%d x %d)" % (self.file_id, self.width, self.height)
+
+
+@python_2_unicode_compatible
+class Document(models.Model):
+    file_id = models.CharField(max_length=255, primary_key=True, verbose_name=_("file_id"))
+    # Optionals
+    thumb = models.ForeignKey(PhotoSize, null=True, blank=True, verbose_name=_("Thumbnail"))
+    file_name = models.CharField(max_length=255, null=True, blank=True, verbose_name=_("File name"))
+    mime_type = models.CharField(max_length=255, null=True, blank=True, verbose_name=_("MIME type"))
+    file_size = models.IntegerField(default=0, null=True, blank=True, verbose_name=_("File size"))
+    class Meta:
+        verbose_name = 'Document'
+        verbose_name_plural = 'Documents'
+    def __str__(self):
+        return "%s (%d %s)" % (self.file_name or self.file_id, self.file_size, _('bytes'))
+
+@python_2_unicode_compatible
+class Sticker(models.Model):
+    file_id = models.CharField(max_length=255, primary_key=True, verbose_name=_("file_id"))
+    width = models.IntegerField(default=0, verbose_name=_("Width"))
+    height = models.IntegerField(default=0, verbose_name=_("Height"))
+    # Optionals
+    thumb = models.ForeignKey(PhotoSize, null=True, blank=True, verbose_name=_("Thumbnail"))
+    emoji = models.CharField(max_length=255, null=True, blank=True, verbose_name=_("Emoji"))
+    file_size = models.IntegerField(default=0, null=True, blank=True, verbose_name=_("File size"))
+    class Meta:
+        verbose_name = 'Sticker'
+        verbose_name_plural = 'Stickers'
+    def __str__(self):
+        return "%s %s (%d x %d)" % (self.emoji or '', self.file_id, self.width, self.height)
+
+@python_2_unicode_compatible
+class Video(models.Model):
+    file_id = models.CharField(max_length=255, primary_key=True, verbose_name=_("file_id"))
+    width = models.IntegerField(default=0, verbose_name=_("Width"))
+    height = models.IntegerField(default=0, verbose_name=_("Height"))
+    duration = models.IntegerField(default=0, verbose_name=_("Duration"))
+    # Optionals
+    thumb = models.ForeignKey(PhotoSize, null=True, blank=True, verbose_name=_("Thumbnail"))
+    mime_type = models.CharField(max_length=255, null=True, blank=True, verbose_name=_("MIME type"))
+    file_size = models.IntegerField(default=0, null=True, blank=True, verbose_name=_("File size"))
+    class Meta:
+        verbose_name = 'Video'
+        verbose_name_plural = 'Videos'
+    def __str__(self):
+        return "%s (%d x %d, %d %s)" % (self.file_id, self.width, self.height, self.duration, _('sec.'))
+
+@python_2_unicode_compatible
+class Voice(models.Model):
+    file_id = models.CharField(max_length=255, primary_key=True, verbose_name=_("file_id"))
+    duration = models.IntegerField(default=0, verbose_name=_("Duration"))
+    # Optionals
+    mime_type = models.CharField(max_length=255, null=True, blank=True, verbose_name=_("MIME type"))
+    file_size = models.IntegerField(default=0, null=True, blank=True, verbose_name=_("File size"))
+    class Meta:
+        verbose_name = 'Voice'
+        verbose_name_plural = 'Voices'
+    def __str__(self):
+        return "%s (%d %s)" % (self.file_id, self.duration, _('sec.'))
+
+@python_2_unicode_compatible
+class Contact(models.Model):
+    phone_number = models.CharField(max_length=50, verbose_name=_("Phone number"))
+    first_name = models.CharField(max_length=255, verbose_name=_("First name"))
+    # Optionals
+    last_name = models.CharField(max_length=255, verbose_name=_("Last name"))
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING, db_constraint=False, verbose_name=_("User"))
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name,) if self.last_name else "%s" % self.first_name
+
+@python_2_unicode_compatible
+class Location(models.Model):
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    class Meta:
+        verbose_name = 'Location'
+        verbose_name_plural = 'Locations'
+    def __str__(self):
+        return "%f, %f" % (self.longitude, self.latitude,)
+
+@python_2_unicode_compatible
+class Venue(models.Model):
+    location = models.ForeignKey(Location)
+    title = models.CharField(max_length=255, verbose_name=_("Title"))
+    address = models.CharField(max_length=255, verbose_name=_("Address"))
+    # Optionals
+    foursquare_id = models.CharField(max_length=255, verbose_name=_("Foursquare identifier"))
+    class Meta:
+        verbose_name = 'Venue'
+        verbose_name_plural = 'Venues'
+    def __str__(self):
+        return "%s" % self.title
+
+@python_2_unicode_compatible
 class Message(models.Model):
 
     message_id = models.BigIntegerField(_('Id'), primary_key=True)
@@ -56,6 +179,16 @@ class Message(models.Model):
     forward_from = models.ForeignKey(User, null=True, blank=True, related_name='forwarded_from',
                                      verbose_name=_("Forward from"))
     text = models.TextField(null=True, blank=True, verbose_name=_("Text"))
+    audio = models.ForeignKey(Audio, null=True, blank=True, verbose_name=_("Audio"))
+    document = models.ForeignKey(Document, null=True, blank=True, verbose_name=_("Document"))
+    photo = models.ManyToManyField(PhotoSize, blank=True, verbose_name=_("Photo"))
+    sticker = models.ForeignKey(Sticker, null=True, blank=True, verbose_name=_("Sticker"))
+    vidoe = models.ForeignKey(Video, null=True, blank=True, verbose_name=_("Video"))
+    voice = models.ForeignKey(Voice, null=True, blank=True, verbose_name=_("Voice"))
+    caption = models.TextField(null=True, blank=True, verbose_name=_("Caption"))
+    contact = models.ForeignKey(Contact, null=True, blank=True, verbose_name=_("Contact"))
+    location = models.ForeignKey(Location, null=True, blank=True, verbose_name=_("Location"))
+    venue = models.ForeignKey(Venue, null=True, blank=True, verbose_name=_("Venue"))
     #  TODO: complete fields with all message fields
 
     class Meta:
@@ -65,7 +198,8 @@ class Message(models.Model):
 
     def __str__(self):
         return "(%s,%s)" % (self.from_user, self.text or '(no text)')
-    
+
+
 class Update(models.Model):
     
     update_id = models.BigIntegerField(_('Id'), primary_key=True)
